64585b7ce14201834dd4cfa33482bd4ef86934d2
Sampada-VS <samssv1@gmail.com>
2020-12-19 03:42:40 +0530

    Added: code to remove contact from addressbook entries

--- js/addressbook_home.js
+++ js/addressbook_home.js
@@ -24,11 +24,21 @@ const createInnerHtml=() =>{
         <td>${addressbookData._state}</td>
         <td>${addressbookData._zip}</td>
         <td>${addressbookData._tel}</td>
-        <td><img id="${addressbookData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-          <img id="${addressbookData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+        <td><img name="${addressbookData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+          <img name="${addressbookData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
         </td>
       </tr>
     `;
     }
     document.querySelector('#table-display').innerHTML=innerHtml;
+}
+const remove= (node) => {
+    let addressbookData=addressbookList.find(bookData => bookData._id == node._id);
+    if(!addressbookData) return;
+    const index=addressbookList.map(bookData => bookData._id)
+                                  .indexOf(addressbookData._id);
+    addressbookList.splice(index,1);
+    localStorage.setItem("AddressbookList",JSON.stringify(addressbookList));
+    document.querySelector(".person-count").textContent=addressbookList.length;
+    createInnerHtml();
 }
